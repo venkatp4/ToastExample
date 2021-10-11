661f537334fb314fe5272b50af69c994f8f58bee
Phanitapu <venkateswarlu.phanitapu@concentrix.com>
2021-10-11 15:36:25 +0530

    Add all my files

---
+++ toastermodule/src/main/java/com/example/toastermodule/ToasterMessage.kt
@@ -0,0 +1,12 @@
+package com.example.toastermodule
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+
+    fun s(c: Context?, message: String?) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
