8bcd4ebf97d7d01fc0442216b40a9e566b7cd354
Phanitapu <venkateswarlu.phanitapu@concentrix.com>
2021-10-11 17:49:22 +0530

    FC

--- app/src/main/java/com/example/toasterproject/MainActivity.kt
+++ app/src/main/java/com/example/toasterproject/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.toasterproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.toastermodule.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        ToasterMessage().s(applicationContext, "test toaster message")
 
 
     }
